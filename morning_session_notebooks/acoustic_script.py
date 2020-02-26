917aa75b715d31ee6c329680221b149af0255c18
Rhodri Nelson <rhodri.nelson@gmail.com>
2020-02-26 16:05:14 +0000

    Complete acoustic example notebook.

---
+++ morning_session_notebooks/acoustic_script.py
@@ -0,0 +1,65 @@
+import numpy as np
+
+from examples.seismic import Model, TimeAxis, RickerSource, Receiver
+from devito import TimeFunction, Eq, solve, Operator, norm
+
+# Define a physical size
+shape = (101, 101)  # Number of grid point (nx, nz)
+spacing = (10., 10.)  # Grid spacing in m. The domain size is now 1km by 1km
+origin = (0., 0.)  # What is the location of the top left corner. This is necessary to define
+                   # the absolute location of the source and receivers
+
+# Define a velocity profile. The velocity is in km/s
+v = np.empty(shape, dtype=np.float32)
+v[:, :51] = 1.5
+v[:, 51:] = 2.5
+
+# With the velocity and model size defined, we can create the seismic model that
+# encapsulates this properties. We also define the size of the absorbing layer as 10 grid points
+model = Model(vp=v, origin=origin, shape=shape, spacing=spacing,
+              space_order=2, nbl=10)
+
+# Acquisition geometry
+t0 = 0.  # Simulation starts a t=0
+tn = 1000.  # Simulation last 1 second (1000 ms)
+dt = model.critical_dt  # Time step from model grid spacing
+
+time_range = TimeAxis(start=t0, stop=tn, step=dt)
+
+f0 = 0.010  # Source peak frequency is 10Hz (0.010 kHz)
+src = RickerSource(name='src', grid=model.grid, f0=f0,
+                   npoint=1, time_range=time_range)
+
+# Set source coordinates
+src.coordinates.data[0, 0] = np.array(model.domain_size[0]) * .5
+src.coordinates.data[0, 1] = 20.  # Depth is 20m
+
+# Create symbol for 101 receivers
+rec = Receiver(name='rec', grid=model.grid, npoint=101, time_range=time_range)
+
+# Prescribe even spacing for receivers along the x-axis
+rec.coordinates.data[:, 0] = np.linspace(0, model.domain_size[0], num=101)
+rec.coordinates.data[:, 1] = 20.  # Depth is 20m
+
+## Finite-difference discretization
+
+# Define the wavefield with the size of the model and the time dimension
+u = TimeFunction(name="u", grid=model.grid, time_order=2, space_order=2)
+
+# We can now write the PDE
+pde = model.m * u.dt2 - u.laplace + model.damp * u.dt
+
+stencil = Eq(u.forward, solve(pde, u.forward))
+
+# Finally we define the source injection and receiver read function to generate the corresponding code
+src_term = src.inject(field=u.forward, expr=src * dt**2 / model.m)
+
+# Create interpolation expression for receivers
+rec_term = rec.interpolate(expr=u.forward)
+
+# Create and run the operator
+op = Operator([stencil] + src_term + rec_term, subs=model.spacing_map)
+op(time=time_range.num-1, dt=model.critical_dt)
+
+# Norm of the receiver data
+print(norm(rec))
